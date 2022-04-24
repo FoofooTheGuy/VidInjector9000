29223129ac26fcb6d19fea578b13f1d1f14f2556
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2022-04-24 16:31:57 -0400

    Add files via upload
    boutta banish the batch file

---
+++ VidInjector9000/build.bat
@@ -0,0 +1,9 @@
+@echo off
+set "input=%1"
+set "output=%~n1"
+echo compiling "%output%.exe"
+del "%output%.exe"
+g++ -Wall -c -g %input% -o obj\Debug\%output%.o
+g++ -static -static-libgcc -static-libstdc++ -o "%output%.exe" obj\Debug\%output%.o
+::g++ -g %input% -o "%output%.exe"
+pause
