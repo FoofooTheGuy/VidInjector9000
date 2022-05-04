5e48f9276c80708acee14c2f28aafca0bb3a07a8
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2022-05-04 15:44:43 -0400

    better builder

--- VidInjector9000/src/build.bat
+++ VidInjector9000/src/build.bat
@@ -2,7 +2,9 @@
 set "input=%1"
 set "output=%~n1"
 echo compiling "%output%.exe"
+rmdir obj\Debug\ /s /q
 del "%output%.exe"
+mkdir obj\Debug
 g++ -Wall -c -g %input% -o obj\Debug\%output%.o
 g++ -static -static-libgcc -static-libstdc++ -o "%output%.exe" obj\Debug\%output%.o
 ::g++ -g %input% -o "%output%.exe"
