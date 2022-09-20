2e9fd57167f0d9e58b886190997706d26505fe53
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2022-09-20 15:27:16 -0400

    bump up version number

--- VidInjector9000/src/VidInjector9001.cpp
+++ VidInjector9000/src/VidInjector9001.cpp
@@ -1147,7 +1147,7 @@ void Settings() {
 		printf("Type a letter:\n\n"
 		" __________________________________________\n"
 		"|                                          |\n"
-		"| Software Version: 2.5.5                  |\n"
+		"| Software Version: 2.5.7                  |\n"
 		"|                                          |\n"
 		"| D: Toggle Debug Information       [");
 							if(Debug) printf("ON]   |\n");
