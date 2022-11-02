1a836e8c69bd44611efbec3655f5ea992f6aa5a9
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2022-11-02 13:16:27 -0400

    specify c++17 because it is

--- VidInjector9000/src/build.bat
+++ VidInjector9000/src/build.bat
@@ -4,8 +4,15 @@ set "output=%~n1"
 echo compiling "%output%.exe"
 rmdir obj\Debug\ /s /q
 del "%output%.exe"
+::del "%output%x86.exe"
 mkdir obj\Debug
-g++ -Wall -c -g %input% -o obj\Debug\%output%.o
+g++ -Wall -c -g %input% -std=c++17 -o obj\Debug\%output%.o
 g++ -static -static-libgcc -static-libstdc++ -o "%output%.exe" obj\Debug\%output%.o
+
+::echo Press any key to build x86. . .
+::pause>NUL
+::g++ -Wall -c -g %input% -std=c++17 -ansi -m32 -o obj\Debug\%output%x86.o
+::g++ -static -static-libgcc -static-libstdc++ -o "%output%x86.exe" obj\Debug\%output%x86.o
+
 ::g++ -g %input% -o "%output%.exe"
 pause
