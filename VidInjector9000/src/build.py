a7f3601c1938170a3177f5bc0e2ee4280870a684
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2022-11-21 21:12:44 -0500

    python cool

---
+++ VidInjector9000/src/build.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import shutil
+import subprocess
+
+bin = "VidInjector9001.exe"
+cpp = "c++17"
+
+try:
+    shutil.rmtree("obj")
+except:
+    print("tree not found")
+
+try:
+    os.remove(bin)
+except:
+    print("failed to delete " + bin)
+
+try:
+    os.makedirs("obj/Debug")
+except:
+    print("failed to create dir")
+
+files = ""
+for s in os.listdir("."):
+    if s.find(".c") != -1:
+        print(s)
+        subprocess.call("g++ -Wall -c -g " + s + " -std=" + cpp + " -o obj/Debug/" + s.rsplit('.', 1)[0] + ".o")
+        files += "obj/Debug/" + s.rsplit('.', 1)[0] + ".o"
+
+subprocess.call("g++ -static -static-libgcc -static-libstdc++ -o \"" + bin + "\" " + files)
