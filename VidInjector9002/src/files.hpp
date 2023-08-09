bf19ae48a6f82050e3d389aadd47e69caae4793f
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2023-08-09 16:14:11 -0400

    fix errors about the array definitions

--- VidInjector9002/src/files.hpp
+++ VidInjector9002/src/files.hpp
@@ -2,6 +2,6 @@
 
 extern std::vector<uint8_t> Multivid;
 extern std::vector<uint8_t> Singlevid;
-extern uint8_t BCWAV_array[112528];
-extern uint8_t M_bmp[462];
-extern uint8_t base_cia[197056];
+extern const uint8_t BCWAV_array[112528];
+extern const uint8_t M_bmp[462];
+extern const uint8_t base_cia[197056];
