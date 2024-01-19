caaa1614a72b1f04b1a4554dedda43260262cac3
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2024-01-19 13:37:49 -0500

    build_deps.py
    build_deps.py

---
+++ VidInjector9002-CLI/build_deps.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+import subprocess
+
+subprocess.call(["git", "clone", "https://github.com/MyPasswordIsWeak/nnc.git"])
+subprocess.call(["git", "clone", "https://github.com/Mbed-TLS/mbedtls.git"])
+try:
+    raw_input("Press Enter to continue...")
+except:
+    input("Press Enter to continue...")
+os.chdir("mbedtls")
+os.environ["CC"] = "gcc"
+subprocess.call(["make"])
+shutil.copyfile("library/libmbedcrypto.a", "../libmbedcrypto.a")
+os.chdir("../nnc")
+print("\n\n\n\nPLEASE READ:\n\nBefore compiling nnc, edit nnc/Makefile in a text editor and add \"-I../mbedtls/include\" (without quotes) to CFLAGS.\n")
+try:
+    raw_input("Press Enter to continue...")
+except:
+    input("Press Enter to continue...")
+subprocess.call(["make"])
+shutil.copyfile("libnnc.a", "../1libnnc.a")
+
+try:
+    raw_input("Press Enter to finish...")
+except:
+    input("Press Enter to finish...")
