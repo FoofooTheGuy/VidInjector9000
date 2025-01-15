80cc04efb4e3dbfb0882da149b2316aca3d4f592
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2025-01-15 11:28:51 -0500

    fix build deps

--- VidInjector9002-CLI/build_deps.py
+++ VidInjector9002-CLI/build_deps.py
@@ -5,9 +5,10 @@ import subprocess
 os.chdir("nnc")
 
 subprocess.call(["cmake", "-B", "build"])
-subprocess.call(["cmake", "--build build", "--config", "Release", "--parallel", "4"])
+subprocess.call(["cmake", "--build", "build", "--config Release", "--parallel 4"])
 
 shutil.copyfile("build/libnnc.a", "../libnnc.a")
+shutil.copyfile("build/extern/mbedtls/library/libmbedcrypto.a", "../libmbedcrypto.a")
 
 try:
     raw_input("Press Enter to finish...")
