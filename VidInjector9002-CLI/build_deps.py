e622d28ba7afe37faabd5378e2f05e18984406fa
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2025-01-15 10:43:19 -0500

    use cmake to build nnc

--- VidInjector9002-CLI/build_deps.py
+++ VidInjector9002-CLI/build_deps.py
@@ -1,21 +1,13 @@
 import os
 import shutil
 import subprocess
-from zipfile import ZipFile
 
-with ZipFile("nnc.zip", 'r') as zObject:
-	zObject.extractall(path=".")
-	
-with ZipFile("mbedtls-3.4.0.zip", 'r') as zObject:
-	zObject.extractall(path=".")
+os.chdir("nnc")
 
-os.chdir("mbedtls-3.4.0")
-os.environ["CC"] = "gcc"
-subprocess.call(["make", "-C", "library"])
-shutil.copyfile("library/libmbedcrypto.a", "../libmbedcrypto.a")
-os.chdir("../nnc")
-subprocess.call(["make"])
-shutil.copyfile("libnnc.a", "../libnnc.a")
+subprocess.call(["cmake", "-B", "build"])
+subprocess.call(["cmake", "--build build", "--config", "Release", "--parallel", "4"])
+
+shutil.copyfile("build/libnnc.a", "../libnnc.a")
 
 try:
     raw_input("Press Enter to finish...")
