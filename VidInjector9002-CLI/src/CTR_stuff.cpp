e77d93ed05fb1ea6fb2ee82fb8690daf90c1672b
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2025-04-24 18:29:52 -0400

    Add cia and tar building info

--- VidInjector9002-CLI/src/CTR_stuff.cpp
+++ VidInjector9002-CLI/src/CTR_stuff.cpp
@@ -1315,38 +1315,58 @@ int build_archive(std::string inVi9p, std::string outCIA, std::string outTAR, ui
 			nnc_vfs exefs;
 			nnc_keypair kp;
 			nnc_seeddb sdb = {};
-
+			
+			std::cout << CIAopenInput << std::endl;
 			TRYB(nnc_file_open(&f, std::string(tempPath + "/base.cia").c_str()), out1); /* open the input file */
+			std::cout << CIAopenOutput << std::endl;
 			TRYB(nnc_wfile_open(&wf, outCIA.c_str()), out2); /* open the output file */
+			std::cout << CIAreadHeader << std::endl;
 			TRYB(nnc_read_cia_header(NNC_RSP(&f), &cia_hdr), out3); /* read the cia header */
+			std::cout << CIAopenCertificate << std::endl;
 			nnc_cia_open_certchain(&cia_hdr, NNC_RSP(&f), &certchain); /* open the certificate chain for later copying it into the new cia */
+			std::cout << CIAopenTicket << std::endl;
 			nnc_cia_open_ticket(&cia_hdr, NNC_RSP(&f), &ticket); /* open the ticket for later copying it into the new cia */
-			nnc_cia_open_tmd(&cia_hdr, NNC_RSP(&f), &tmd_strm); /* open the tmd which we will modify some things of and then write tot he new cia */
+			std::cout << CIAopenTMD << std::endl;
+			nnc_cia_open_tmd(&cia_hdr, NNC_RSP(&f), &tmd_strm); /* open the tmd which we will modify some things of and then write to the new cia */
+			std::cout << CIAparseTicket << std::endl;
 			TRYB(nnc_read_tmd_header(NNC_RSP(&tmd_strm), &tmd), out3); /* parse the ticket */
+			std::cout << CIAcreateReader << std::endl;
 			TRYB(nnc_cia_make_reader(&cia_hdr, NNC_RSP(&f), nnc_get_default_keyset(), &reader), out3); /* create a content (= NCCH) reader */
+			std::cout << CIAopenFirstContent << std::endl;
 			TRYB(nnc_cia_open_content(&reader, 0, &ncch0stream, NULL), out4); /* open the first content (NCCH0) */
+			std::cout << CIAparseNCCH << std::endl;
 			TRYB(nnc_read_ncch_header(NNC_RSP(&ncch0stream), &ncch_hdr), out5); /* parse the NCCH header */
 
+			std::cout << CIAinitializeVFS << std::endl;
 			TRYB(nnc_vfs_init(&romfs), out5); /* initialize a VFS */
+			std::cout << CIApopulateVFS << " (RomFS)" << std::endl;
 			TRYB(nnc_vfs_link_directory(&romfs.root_directory, romfsPath.c_str(), nnc_vfs_identity_transform, NULL), out6); /* populate the VFS, another source of files could be a RomFS, see #nnc_romfs_to_vfs */
+			std::cout << CIAinitializeVFS << std::endl;
 			TRYB(nnc_vfs_init(&exefs), out5); /* initialize a VFS */
+			std::cout << CIApopulateVFS << " (ExeFS)" << std::endl;
 			TRYB(nnc_vfs_link_directory(&exefs.root_directory, std::string(tempPath + "/exefs").c_str(), nnc_vfs_identity_transform, NULL), out10);
 
+			std::cout << CIAscanSeeddb << std::endl;
 			if ((res = nnc_scan_seeddb(&sdb)) != NNC_R_OK) /* scan for a seeddb for use with "new crypto" and set it as the default */
 				nnc_set_default_seeddb(&sdb);
+			std::cout << CIAgenerateKeys << std::endl;
 			TRYB(nnc_fill_keypair(&kp, nnc_get_default_keyset(), nnc_get_default_seeddb(), &ncch_hdr), out7); /* generate the cryptographic keys for if the NCCH is encrypted */
+			std::cout << CIAopenExheader << std::endl;
 			if (nnc_file_open(&exheader, std::string(tempPath + "/exheader.bin").c_str()) != NNC_R_OK) {/* open exheader file */
 				std::cout << ErrorText << ' ' << FailedToReadFile << " \"" << tempPath << "/exheader.bin\"" << std::endl;
 				goto out10;
 			}
 			nnc_exheader exhdr;
+			std::cout << CIAreadExheader << std::endl;
 			if (nnc_read_exheader(NNC_RSP(&exheader), &exhdr) != NNC_R_OK) {
 				std::cout << ErrorText << ' ' << FailedToReadExHeader << " \"" << tempPath << "/exheader.bin\"" << std::endl;
 				goto out10;
 			}
+			std::cout << CIAopenLogo << std::endl;
 			TRYB(nnc_ncch_section_logo(&ncch_hdr, NNC_RSP(&ncch0stream), &logo), out9); /* logo stream */
 
 			/* setup the parameters for building, for more options see the documentation. */
+			std::cout << CIAsetParameters << std::endl;
 			ncch0.type = NNC_CIA_NCCHBUILD_BUILD;
 			ncch0.ncch = &ncch0b;
 			nnc_condense_ncch(&ncch0b.chdr, &ncch_hdr);
@@ -1371,6 +1391,7 @@ int build_archive(std::string inVi9p, std::string outCIA, std::string outTAR, ui
 			tmd.content_count = 1;
 			tmd.title_id = exhdr.title_id;
 			{
+				std::cout << CIAsetTitleID << std::endl;
 				//change the title ID of the ticket
 				std::vector<char> ticket_contents = std::vector<char>(NNC_RS_CALL0(ticket, size));
 				nnc_u32 out_size = 0;
@@ -1386,12 +1407,14 @@ int build_archive(std::string inVi9p, std::string outCIA, std::string outTAR, ui
 				}
 				nnc_mem_open(&modified_ticket, ticket_contents.data(), NNC_RS_CALL0(ticket, size));
 
+				std::cout << CIAwriteCIA << std::endl;
 				/* and finally write the cia */
 				res = nnc_write_cia(
 					NNC_CIA_WF_CERTCHAIN_STREAM | NNC_CIA_WF_TICKET_STREAM | NNC_CIA_WF_TMD_BUILD,
 					&certchain, &modified_ticket, &tmd, 1, &ncch0, NNC_WSP(&wf)
 				);
 			}
+		std::cout << CIAcleanUp << std::endl;
 		/* cleanup code, with lots of labels to jump to in case of failure depending on where it failed */
 		out10: nnc_vfs_free(&exefs);
 		out9: NNC_RS_CALL0(exheader, close);
@@ -1409,9 +1432,10 @@ int build_archive(std::string inVi9p, std::string outCIA, std::string outTAR, ui
 				return res;
 			}
 		}
-		else {
+		else {//TAR creation
 			std::cout << CreatingFile << ' ' << outTAR.substr(outTAR.find_last_of("/\\") + 1) << std::endl;
 			
+			std::cout << TARremoveExisting << std::endl;
 			if (std::filesystem::exists(std::filesystem::path((const char8_t*)&*outTAR.c_str()), error)) {//write wont work properly if it already exists
 				std::filesystem::remove_all(std::filesystem::path((const char8_t*)&*outTAR.c_str()), error);
 				if (error) {
@@ -1423,6 +1447,7 @@ int build_archive(std::string inVi9p, std::string outCIA, std::string outTAR, ui
 			
 			mtar_t tar;
 
+			std::cout << TARopenArchive << std::endl;
 			/* Open archive for writing */
 			mtar_open(&tar, outTAR.c_str(), "wb");
 
@@ -1437,9 +1462,11 @@ int build_archive(std::string inVi9p, std::string outCIA, std::string outTAR, ui
 				std::cout << ErrorText << ' ' << FailedToCreateFile << " \"" << outTAR << '\"' << std::endl;
 			}
 
+			std::cout << TARfinalize << std::endl;
 			/* Finalize -- this needs to be the last thing done before closing */
 			mtar_finalize(&tar);
 
+			std::cout << TARclose << std::endl;
 			/* Close archive */
 			mtar_close(&tar);
 		}
