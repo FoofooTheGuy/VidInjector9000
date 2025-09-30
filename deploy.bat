11ebe0e9e80a946d543366b53c1070973a735ed6
Foofoo_The_Guy <32585652+FoofooTheGuy@users.noreply.github.com>
2025-09-30 13:55:18 -0400

    copy instead of zip

--- deploy.bat
+++ deploy.bat
@@ -14,8 +14,6 @@ if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 cmake --build build --config %BUILD_TYPE% --parallel 4
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-set "ZIP=Windows.zip"
-
 mkdir "build/%BUILD_TYPE%/VidInjector9000Resources"
 robocopy "VidInjector9000Resources/image" "build/%BUILD_TYPE%/VidInjector9000Resources/image" /e
 
@@ -33,19 +31,10 @@ robocopy "build/%BUILD_TYPE%" "../VidInjector9002.5/build/%BUILD_TYPE%/VidInject
 
 cd "../VidInjector9002.5/build/%BUILD_TYPE%"
 
-echo zip binary...
-title zip binary...
+echo copy out binary...
+title copy out binary...
 
-SETLOCAL EnableDelayedExpansion
 for /f "delims=" %%i in ('dir /b') do (
-	set "FILES=!FILES! %%i"
+	robocopy . ../../../out %%i /e
 )
-:: remove leading whitespace
-for /f "tokens=*" %%i in ("%FILES%") do (
-    set "FILES=%%i"
-)
-
-echo "%FILES%" -^> "../../../out/%ZIP%"
-
-tar.exe -a -c -f "../../../out/%ZIP%" %FILES%
 pause
--- deploy.sh
+++ deploy.sh
@@ -22,22 +22,20 @@ cd 'build'
 # get the path of the binary since on mac it is a package
 BIN90025=$(find './' -maxdepth 1 -not -name '.' -not -name 'CMakeFiles' -not -name 'resources' -not -name 'wxWidgets-*' -not -name 'CMakeCache.txt' -not -name 'Makefile' -not -name 'cmake_install.cmake' -not -name 'VidInjector9000Resources')
 MAC=false
-ZIP='Linux.zip'
 
 # on mac, the package is a dir
 if [[ -d $BIN90025 ]]; then
 	MAC=true
-	ZIP='MacOS.zip'
 fi
 
 cd ..
 
 if [[ $MAC = true ]]; then
 	mkdir "build/${BIN90025}/Contents/MacOS/VidInjector9000Resources"
-	cp -r 'VidInjector9000Resources/image' "build/${BIN90025}/Contents/MacOS/VidInjector9000Resources/"
+	cp -a 'VidInjector9000Resources/image' "build/${BIN90025}/Contents/MacOS/VidInjector9000Resources/"
 else
 	mkdir 'build/VidInjector9000Resources'
-	cp -r 'VidInjector9000Resources/image' 'build/VidInjector9000Resources/'
+	cp -a 'VidInjector9000Resources/image' 'build/VidInjector9000Resources/'
 fi
 
 cd '../VidInjector9002-CLI'
@@ -56,15 +54,15 @@ if [[ "$ret" -ne 0 ]]; then
 fi
 
 if [[ $MAC = true ]]; then
-	cp 'build/VidInjector9002-CLI' "../VidInjector9002.5/build/${BIN90025}/Contents/MacOS/VidInjector9000Resources"
+	cp -a 'build/VidInjector9002-CLI' "../VidInjector9002.5/build/${BIN90025}/Contents/MacOS/VidInjector9000Resources"
 else
-	cp 'build/VidInjector9002-CLI' '../VidInjector9002.5/build/VidInjector9000Resources'
+	cp -a 'build/VidInjector9002-CLI' '../VidInjector9002.5/build/VidInjector9000Resources'
 fi
 
 cd '../VidInjector9002.5/build'
 
-echo 'zip binary...'
+echo 'copy out binary...'
 
-zip -r "../../out/${ZIP}" $(find './' -maxdepth 1 -not -name '.' -not -name 'CMakeFiles' -not -name 'resources' -not -name 'wxWidgets-*' -not -name 'CMakeCache.txt' -not -name 'Makefile' -not -name 'cmake_install.cmake')
+cp -a $(find './' -maxdepth 1 -not -name '.' -not -name 'CMakeFiles' -not -name 'resources' -not -name 'wxWidgets-*' -not -name 'CMakeCache.txt' -not -name 'Makefile' -not -name 'cmake_install.cmake') ../../out/
 
 echo 'finished'
